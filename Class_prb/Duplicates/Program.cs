a1494526b0ff16a23f64aeb01db781330e460fba
Vallem Balu <vbnreddy5@gmail.com>
2021-09-18 10:29:25 +0530

    Fibonacci seriesand Factorial programs Iterative Approach and Recursion

--- Class_prb/Duplicates/Program.cs
+++ Class_prb/Duplicates/Program.cs
@@ -9,22 +9,52 @@ namespace Duplicates
         static void Main(string[] args)
         {
 
-            Console.WriteLine("\nOnly Duplicate elements in given array:");
-            int[] arr = new int[]{20, 20, 30, 40, 50, 50, 50};
-            //string[] arr = new string[] { "bcd", "abd", "jude", "bcd", "oiu", "gzw", "oiu" };
-            for (int i = 0; i < arr.Length; i++)
-            {
-                for (int j = i + 1; j < arr.Length; j++)
-                {
-                    if (arr[i] == arr[j])
-                        Console.Write(arr[j]+" ");
+            //Console.WriteLine("\nOnly Duplicate elements in given array:");
+            //int[] arr = new int[]{20, 20, 30, 40, 50, 50, 50};
+            ////string[] arr = new string[] { "bcd", "abd", "jude", "bcd", "oiu", "gzw", "oiu" };
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    for (int j = i + 1; j < arr.Length; j++)
+            //    {
+            //        if (arr[i] == arr[j])
+            //            Console.Write(arr[j]+" ");
+            //    }
+            //}
+            //Console.WriteLine("\n\nRemove Duplicate elements in given array:");
+            //var sList = new ArrayList();
 
-                }
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (sList.Contains(arr[i]) == false)
+            //    {
+            //        sList.Add(arr[i]);
+            //    }
+            //}
+            //var sNew = sList.ToArray();
+            //for (int i = 0; i < sNew.Length; i++)
+            //{
+            //    Console.Write(sNew[i]+" ");
+            //}
+
+            //Enter type array
+            Console.WriteLine("Enter the size of array: ");
+            int length_arr = Convert.ToInt32(Console.ReadLine());
+            int i;
+            int[] arr = new int[length_arr];
+            for (i = 0; i < length_arr; i++)
+            {
+                Console.Write("Enter your number: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
             }
-            Console.WriteLine("\n\nRemove Duplicate elements in given array:");
+            Console.WriteLine("\n");
+            for (i = 0; i < length_arr; i++)
+            {
+                Console.WriteLine("Array values {0}", arr[i]);
+            }
+            Console.ReadLine();
             var sList = new ArrayList();
 
-            for (int i = 0; i < arr.Length; i++)
+            for ( i = 0; i < length_arr; i++)
             {
                 if (sList.Contains(arr[i]) == false)
                 {
@@ -32,9 +62,9 @@ namespace Duplicates
                 }
             }
             var sNew = sList.ToArray();
-            for (int i = 0; i < sNew.Length; i++)
+            for ( i = 0; i < sNew.Length; i++)
             {
-                Console.Write(sNew[i]+" ");
+                Console.Write(sNew[i] + " ");
             }
 
         }
---
+++ Class_prb/Fibonacci_seriesandFactorialprogram/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Fibonacci_seriesandFactorialprogram
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibonacci seriesand Factorial programs Iterative Approach and Recursion");
+
+            Console.WriteLine("\nFactorial");
+            //Factorial Iterative Approach
+            int i1, fact = 1, number1;
+            Console.Write("Enter any Number: ");
+            number1 = int.Parse(Console.ReadLine());
+            for (i1 = 1; i1 <= number1; i1++)
+            {
+                fact = fact * i1;
+            }
+            Console.Write("Factorial of " + number1 + " is: " + fact);
+
+            //Recursion
+            long fact1 = GetFactorial(number1);
+            Console.WriteLine("\nRecursion factorial of {0}  is {1}", number1, fact1);
+            Console.ReadKey();
+
+            Console.WriteLine("\nFibonacci_series");
+            //Iterative Approach
+            int n1 = 0, n2 = 1, n3, i, number;
+            Console.WriteLine("\nEnter the number of elements: ");
+            number = int.Parse(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " "); //printing 0 and 1    
+            for (i = 2; i <= number; ++i) //loop starts from 2 because 0 and 1 are already printed    
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+
+            //Recursion
+            Console.Write("\n\nFibonacci_series in Recursion {0} th value: ", number);
+            Console.Write(Fib(number));
+            Console.WriteLine("\n\nFibonacci_series in Recursion values: ");
+            for (i = 0; i <= number; i++)
+            {
+                Console.Write("{0} ", Fib(i));
+            }
+        }
+        private static int Fib(int number)
+        {
+            if (number <= 1)
+            {
+                return number;
+            }
+            else
+            {
+                return Fib(number - 1) + Fib(number - 2);
+            }
+        }
+        private static long GetFactorial(int number1)
+        {
+            if (number1 == 0)
+            {
+                return 1;
+            }
+
+            return number1 * GetFactorial(number1 - 1);
+        }
+    }
+}
