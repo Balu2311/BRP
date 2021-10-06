d22871924023f7cd682e30f7df9d49d907ed2584
Vallem Balu <vbnreddy5@gmail.com>
2021-10-06 08:02:05 +0530

    matrix and sum and multiply

--- Class_prb/Add_matrics/Program.cs
+++ Class_prb/Add_matrics/Program.cs
@@ -74,7 +74,28 @@ namespace Add_matrics
                 Console.WriteLine();
             }
             Console.ReadLine();
+
+            //Multiply matrics
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    arr3[i, j] = arr1[i, j] * arr2[i, j];
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\nMultiply two matrics:");
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    Console.Write(arr3[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
+
         
     }
 }
