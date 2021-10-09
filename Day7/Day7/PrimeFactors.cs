e22d32ed58aa2d60ab126ca070f11fd8e55e148c
Vallem Balu <vbnreddy5@gmail.com>
2021-10-09 10:49:19 +0530

    Day7 programs

---
+++ Day7/Day7/PrimeFactors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day7
+{
+    class PrimeFactors
+    {
+        public void findPrimeFactors()
+        {
+            int Num;
+            Console.WriteLine("Enter Number of which you want to print factors =");
+            Num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Prime factors are ");
+            for (int i = 2; i < Num; i++)
+            {
+                if (Num % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ Day7/Day7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day7 Programs");
+            PrimeFactors primeFactors = new PrimeFactors();
+            primeFactors.findPrimeFactors();
+
+            SecondLargestAndSmallest secondLargest = new SecondLargestAndSmallest();
+            secondLargest.FindSecondLargest();
+
+
+            Binary binary = new Binary();
+            binary.findBinary();
+        }
+    }
+}
