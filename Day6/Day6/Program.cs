ac009672bae5ba9b320f43ebe548a741b862e2fa
Vallem Balu <vbnreddy5@gmail.com>
2021-10-09 10:41:37 +0530

    Day6 programs

---
+++ Day6/Day6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day6 Pregrams");
+            Console.WriteLine("Prime Numbers");
+            PrimeNumber p = new PrimeNumber();
+            p.PrintPrimeNumber();
+
+            Console.WriteLine("\nRollDie");
+            RollDie roll = new RollDie();
+            roll.findMaximum();
+            Console.WriteLine("\nRepeated number");
+            RepeatedNumber repeatedNumber = new RepeatedNumber();
+            repeatedNumber.FindRepeatedNumber();
+        }
+    }
+}
