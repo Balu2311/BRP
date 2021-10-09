d33b1c50764d8cdc16670e7ed2bbe7be43d3ffc3
Vallem Balu <vbnreddy5@gmail.com>
2021-10-09 09:57:09 +0530

    Day 4 programs

---
+++ Day4/Day4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            Console.WriteLine("Enter input \n" +
+                "1.Temperature Conversion \n" +
+                "2.Square root of a non -negative c\n" +
+                "3.Harmoic series \n" +
+                "4.Trignometric function");
+
+            input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+
+            switch (input)
+            {
+
+                case 1:
+                    temp_convertion t = new temp_convertion();
+                    t.convertTemperature();
+                    break;
+
+                case 2:
+                    sqrt s = new sqrt();
+                    s.caculateSquareRoot();
+                    break;
+
+                case 3:
+                    Harmonic h = new Harmonic();
+                    h.findHarmonicSeries();
+                    break;
+
+                case 4:
+                   trigs T = new trigs();
+                    T.TrignometricFunctions();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day4/Day4/sqrt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4
+{
+    class sqrt
+    {
+        public void caculateSquareRoot()
+        {
+
+            Console.WriteLine("Finding the square root of non-negative number");
+            double C;
+            Console.WriteLine("Enter the input C");
+            C = Convert.ToDouble(Console.ReadLine());
+            double t = C;
+            double epsilon = 1e-15;
+            while (Math.Abs(t - C / t) > epsilon * t)
+            {
+                t = (C / t + t) / 2.0;
+            }
+            Console.WriteLine("The value of t is : " + t);
+            Console.ReadLine();
+
+        }
+    }
+}
