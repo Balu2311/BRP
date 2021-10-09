ad49c290b4ee0ef9ce579717eb607a75920efa19
Vallem Balu <vbnreddy5@gmail.com>
2021-10-09 10:07:33 +0530

    Day5 programs

---
+++ Day5/Day5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day5 progras");
+            FlipCoin f = new FlipCoin();
+            f.calculatePercentage();
+
+            PowerOFTwo p = new PowerOFTwo();
+            p.printPowerOfTwo();
+
+
+            GamblingSimulator gamblingSimulator = new GamblingSimulator();
+            gamblingSimulator.findNumberOfWin();
+
+            Console.ReadLine();
+        }
+    }
+}
